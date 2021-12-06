1b577c1994f1a0a3942018f64518bd6d22aa624f
PhuongNguyen <phuongnguyenhoang13th@gmail.com>
2021-12-06 22:12:30 +0700

    update 2 for ...>

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p> asm1 updated
+        <p> asm1 updated 2
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
