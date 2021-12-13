ebfb7d29076dce901223833c2d504be9cf771a39
PhuongNguyen <phuongnguyenhoang13th@gmail.com>
2021-12-13 23:24:51 +0700

    C3 done coding and fixing

--- src/StaffListComponets/EEdetailComponent.js
+++ src/StaffListComponets/EEdetailComponent.js
@@ -1,23 +1,26 @@
-function EedetailComponet(props){
-    console.log(props.SelectedEe)
-    const SelectedEe = props.SelectedEe.map((SelectedStaff)=>{
-            return (
-                <div key={SelectedStaff.key}>
-                    <h1>Họ và Tên : {SelectedStaff.name}</h1>
-                    <ul>
-                        <li><p>Ngày Sinh : {SelectedStaff.doB}</p></li>
-                        <li><p>Ngày vào Công ty : {SelectedStaff.startDate}</p></li>
-                        <li><p>Phòng Ban : {SelectedStaff.department}</p></li>
-                        <li><p>Số ngày nghỉ còn lại : {SelectedStaff.annualLeave}</p></li>
-                        <li><p>Số ngày đã làm thêm : {SelectedStaff.overTime}</p></li>
-                    </ul>
-                </div>
-            )
-    })
-    return(
-        <div>
-            {SelectedEe}
-        </div>
-    )
+function EedetailComponent(props){
+    console.log(props.selectedStaff)
+    if(props.selectedStaff!==null){
+                return (
+
+                    <div key={props.selectedStaff.id}>
+                        <h1>Họ và Tên : {props.selectedStaff.name}</h1>
+                        <ul>
+                            <li><p>Ngày Sinh : {(new Date(props.selectedStaff.doB)).toLocaleString('vi-VN')}</p></li>
+                            <li><p>Ngày vào Công ty : {(new Date(props.selectedStaff.startDate)).toLocaleString('vi-VN')}</p></li>
+                            <li><p>Phòng Ban : {props.selectedStaff.department.name}</p></li>
+                            <li><p>Số ngày nghỉ còn lại : {props.selectedStaff.annualLeave}</p></li>
+                            <li><p>Số ngày đã làm thêm : {props.selectedStaff.overTime}</p></li>
+                        </ul>
+                    </div>
+                )
+        }
+
+    else{
+        return(
+           <div></div>
+        )
+    }
+
 }
-export default EedetailComponet
+export default EedetailComponent
