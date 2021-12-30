46dab631c45a748a57a61b5a3590cf77ac5b0d25
PhuongNguyen <phuongnguyenhoang13th@gmail.com>
2021-12-30 11:18:16 +0700

    Initiate asm3 as well as add search bar

---
+++ src/components/Eelist.js
@@ -0,0 +1,40 @@
+import { Card,CardImg,CardImgOverlay,CardText,CardBody,CardTitle } from 'reactstrap'
+import { useState } from "react"
+import { Link } from 'react-router-dom';
+import {Navbar,NavDropdown,NavItem,Container,Nav,Form,FormControl,Button} from 'react-bootstrap'
+import logo from '../assets/images/alberto.png'
+function Eelist(props){
+    const[selectedEe,setSelectedEe] = useState(null)
+    const OnSelectedEe = (staffs)=>{
+        setSelectedEe(staffs);
+    }
+    const Liststaff = props.staffs.map((staffs)=>{
+        return(
+            <div key={staffs.id} onClick={()=>OnSelectedEe(staffs)} className='col-sm-12 col-md-6 col-lg-3 mt-5'>
+                <Link className='text-decoration-none text-dark' to={`/${staffs.id}`}>
+                    <Card body className="text-center">
+                        <CardImg  width='100%' src={logo} alt={staffs.name}/>
+                        <CardTitle >{staffs.name}</CardTitle>
+                    </Card>
+                </Link>
+            </div>
+        )
+    })
+    return (
+        <div className='container mt-5'>
+            <div className='row'>
+                <Form className="d-flex">
+                    <FormControl
+                    type="search"
+                    placeholder="Search"
+                    className="me-2"
+                    aria-label="Search"
+                    />
+                    <Button variant="outline-light">Search</Button>
+                </Form>
+                {Liststaff}
+            </div>
+        </div>
+    )
+}
+export default Eelist
